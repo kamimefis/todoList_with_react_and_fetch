c2d2c66392f3a3c3b0c45835f2639c588535a787
Camila <camilalobos.2307@gmail.com>
2020-10-14 18:33:46 -0300

    todoList using an api

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -6,73 +6,162 @@ class TodoInput extends Component {
     constructor() {
         super();
         this.state = {
-            items: [],   //almacenará los items agregados a la lista
-            currentItem: { text: "", id: "" }
+            items: [],
+            currentItem: { label: "", done: "false" }
         }
         this.handleInput = this.handleInput.bind(this);
         this.addItem = this.addItem.bind(this);
         this.deleteItem = this.deleteItem.bind(this);
     }
-    
-    //Método que maneja lo que se escribe en el input(recibe el mensaje escrito)
-    handleInput = e => {
+
+    componentDidMount() {
+        // send HTTP request
+        // save it to the state
+        const additionalSetting = {
+            headers: { "Content-Type": "application/json" },
+            method: "GET",
+        }
+        fetch (
+            "https://assets.breatheco.de/apis/fake/todos/user/camilin",
+            additionalSetting)
+            .then(response => response.json())
+            .then(newResponse => {
+                console.log(newResponse);
+                // JSON a TEXTO   = JSON.stringify()
+                // TEXTO A JSON   = JSON.parse()
+                this.setState({ items: newResponse })
+            })
+            .catch((error) => console.log(error));
+
+        console.log(this.state.items);
+    };
+    //PUT method:
+    putMethod = (e) => {
+        e.preventDefault();
+        //console.log([...this.state.items, this.state.currentItem])
+        const additionalSetting = {
+            headers: {
+                "Content-Type": "application/json"
+            },
+            method: "PUT",
+            body: JSON.stringify([...this.state.items, this.state.currentItem])
+        }
+        fetch(
+            "https://assets.breatheco.de/apis/fake/todos/user/camilin",
+            additionalSetting)
+            .then(response => response.json())
+            .then(newResponse => {
+                console.log(newResponse);
+            })
+            .catch((error) => console.log(error));
         this.setState({
-            currentItem: { text: e.target.value, id: Date.now() }
+            items: [...this.state.items, this.state.currentItem],
+            currentItem: { label: "", done: "" } //clear the input field to add a new task
         })
     }
-    //Método para agregar el mensaje de input a la lista
+    //DELETE method:
+    deleteMethod = (i) => {
+        let array = this.state.items.filter((item, index) => i !== index);
+        //console.log(array);
+        const additionalSetting = {
+            headers: {
+                "Content-Type": "application/json"
+            },
+            method: "PUT",
+            body: JSON.stringify([...array])
+        }
+        fetch(
+            "https://assets.breatheco.de/apis/fake/todos/user/camilin",
+            additionalSetting)
+            .then(response => response.json())
+            .then(newResponse => {
+                console.log(newResponse);
+            })
+            .catch((error) => console.log(error));
+        this.setState({ items: [...array] })
+    }
+    handleInput = e => {
+        this.setState({
+            currentItem: { label: e.target.value, done: true }
+        })
+    }
     addItem = e => {
         e.preventDefault();
         const newItem = this.state.currentItem;
-        if (newItem.text !== "") {
+        if (newItem.label !== "") {
             const items = [...this.state.items, newItem];
             this.setState({
                 items: items,
-                currentItem: {  //Para que al enviar el item(tarea) se vacíe el input
-                    text: '',
-                    id: ''
+                currentItem: {  //clear the input field
+                    label: '',
+                    done: ''
                 }
             })
         }
     }
-    //Método para eliminar un item utilizando filter method
     deleteItem = (id) => {
         console.log(this.state);
         const deletedItems = this.state.items.filter(item => item.id !== id);
         this.setState({
             items: deletedItems
         })
-        console.log(deletedItems);
+        //console.log(deletedItems);
     }
-   
+    deleteAllTasks = i => {
+        let array = [this.state.items];
+        const additionalSetting = {
+            headers: {
+                "Content-Type": "application/json"
+            },
+            method: "PUT",
+            body: JSON.stringify([...array])
+        }
+        fetch(
+            "https://assets.breatheco.de/apis/fake/todos/user/camilin",
+            additionalSetting)
+            .then(response => response.json())
+            .then(newResponse => {
+                console.log(newResponse);
+            })
+            .catch((error) => console.log(error));
+        this.setState({ items: [] })
+    }
+
     render() {
         return (
             <div className="container  mt-4">
                 {/*title*/}
                 <p className="todo-title text-center font-weight-light">todo list</p>
-                
+
                 <div className="list-group col-8 m-auto">
                     {/*Form---ADD an item to input field*/}
-                    <form onSubmit={this.addItem}>
+                    <form onSubmit={this.putMethod}>
                         <input
                             className="form-control"
                             type="text"
                             placeholder="Add a task"
-                            value= {this.state.currentItem.text}
+                            value={this.state.currentItem.label}
                             onChange={this.handleInput}
                         />
-                        <button className="btn d-none" type="submit"></button>
                     </form>
+
                     {/*List of tasks and delete button*/}
-                    <TodoList items={this.state.items} deleteItem={this.deleteItem} />
+                    <TodoList items={this.state.items} deleteMethod={this.deleteMethod}
+                    />
 
                     {/*Tasks counter*/}
                     <div className="item-counter list-group bg-white border pt-2 pl-3">
                         <p>{this.state.items.length} item left</p>
                     </div>
-                    
-                </div>
 
+                    {/*Delete all Tasks in the list */}
+                    <button
+                        onClick={this.deleteAllTasks}
+                        type="button"
+                        className="btn btn-outline-danger btn-lg btn-block">
+                        Delete all tasks
+                    </button>
+                </div>
             </div>
         );
     }
